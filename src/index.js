d71c3a3e223d7000a9c089de9adf130934bd5dec
EasonLLin <jack2872@hotmail.com>
2019-08-05 22:56:49 +0800

    feat: remove react-router-dom code, use react-router instead

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+// import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render((
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-), document.getElementById('root'));
+// below code use react-router-dom but should use rect-router
 
-// ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render((
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// ), document.getElementById('root'));
 
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'))
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
