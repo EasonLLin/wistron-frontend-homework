2d4b5a8923e9811dab267ec8adc1938982ae104b
EasonLLin <jack2872@hotmail.com>
2019-08-05 23:41:22 +0800

    feat: add login show page

---
+++ src/views/Login/LoginShow.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class LoginShow extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>Sign in</button>
+      </div>
+    )
+  }
+}
+
+export default LoginShow
