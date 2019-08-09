a700e0e514b32e71255be73cebbc6c9671637848
EasonLLin <jack2872@hotmail.com>
2019-08-09 12:24:49 +0800

    Updates

---
+++ precache-manifest.8898b2072c416751488934209cfdedf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87987565246f27cc5e2a80fef08ef92a",
+    "url": "/wistron/index.html"
+  },
+  {
+    "revision": "17e7795812be144bfd54",
+    "url": "/wistron/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a400808ffad29f04e8af",
+    "url": "/wistron/static/js/2.2271ec4c.chunk.js"
+  },
+  {
+    "revision": "17e7795812be144bfd54",
+    "url": "/wistron/static/js/main.cc9b4260.chunk.js"
+  },
+  {
+    "revision": "049a3d3d9378751e65b5",
+    "url": "/wistron/static/js/runtime~main.a337fc34.js"
+  }
+]);
