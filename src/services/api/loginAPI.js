d2bee757ffaa434f765a9879374e42cd6cfa449c
EasonLLin <jack2872@hotmail.com>
2019-08-12 02:15:34 +0800

    feat: delay axios response 3000ms

--- src/services/api/loginAPI.js
+++ src/services/api/loginAPI.js
@@ -1,8 +1,8 @@
 import axios from './index.js'
 import MockAdapter from 'axios-mock-adapter'
 
-// var mock = new MockAdapter(axios, { delayResponse: 3000 })
-var mock = new MockAdapter(axios)
+var mock = new MockAdapter(axios, { delayResponse: 3000 })
+// var mock = new MockAdapter(axios)
 
 mock.onPost('/api/login').reply(function(config) {
   const data = JSON.parse(config.data)
